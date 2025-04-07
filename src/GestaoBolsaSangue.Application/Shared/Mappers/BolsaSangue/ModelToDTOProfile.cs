f4f6354ceae1a67df6765ef002811064606be537
Leticia Lauriano da Silva <leticia_lauriano@hotmail.com>
2025-04-07 00:34:59 -0300

    Adicionando Mapper

--- src/GestaoBolsaSangue.Application/Shared/Mappers/BolsaSangue/ModelToDTOProfile.cs
+++ src/GestaoBolsaSangue.Application/Shared/Mappers/BolsaSangue/ModelToDTOProfile.cs
@@ -12,6 +12,12 @@ namespace GestaoBolsaSangue.Application.Shared.Mappers.BolsaSangue
                 .ForPath(d => d.Proprietario.Id, opt => opt.MapFrom(s => s.IdProprietario))
                 .ForPath(d => d.Localizacao.Id, opt => opt.MapFrom(s => s.IdLocalizacao));
 
+            CreateMap<DTOs.Alterar.AlterarBolsaSangueDTO, Domain.Models.BolsaSangue>()
+                .ForPath(d => d.Animal.Id, opt => opt.MapFrom(s => s.IdAnimal))
+                .ForPath(d => d.Tipo.Id, opt => opt.MapFrom(s => s.IdTipoBolsa))
+                .ForPath(d => d.Proprietario.Id, opt => opt.MapFrom(s => s.IdProprietario))
+                .ForPath(d => d.Localizacao.Id, opt => opt.MapFrom(s => s.IdLocalizacao));
+
             CreateMap<Domain.Models.Animal, DTOs.BolsaSangue.Listar.AnimalDTO>().ReverseMap();
             CreateMap<Domain.Models.Proprietario, DTOs.BolsaSangue.Listar.ProprietarioDTO>().ReverseMap();
             CreateMap<Domain.Models.Localizacao, DTOs.BolsaSangue.Listar.LocalizacaoDTO>().ReverseMap();
